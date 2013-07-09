039a8140674c6b518671e2eeb090f641de657162
bert <wislan@student.agh.edu.pl>
2013-07-09 19:27:11 +0100

    index with buffer

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+var fs = require('fs');
+var buffer= New Buffer(256);
+response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));  
 });
 
 var port = process.env.PORT || 5000;
